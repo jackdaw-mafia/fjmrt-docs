976edc8ec87817c7f519655a96d87672424130eb
ricojamjar <44123869+ricojamjar@users.noreply.github.com>
2019-07-12 12:41:24 +0100

    here's some dummy data to use
    please update 'duration' to whatever that will be IE database timestamp

---
+++ deals-data.js
@@ -0,0 +1,48 @@
+export const dealsObj = {
+  dealsArr: [
+    {
+      price: "£5",
+      drink: "House Lager",
+      quantity: "2 Pints",
+      name: "The Pilcrow",
+      timerImg: require("../images/timer.png"),
+      venueImg: require("../images/pilcrowIcon.jpg"),
+      type: "🍺",
+      duration: 850,
+      couponID: 12345
+    },
+    {
+      price: "£10",
+      drink: "House Wine",
+      quantity: "1 Bottle",
+      name: "Terrace",
+      timerImg: require("../images/timer.png"),
+      venueImg: require("../images/TerraceIcon.jpg"),
+      type: "🍷",
+      duration: 900,
+      couponID: 12346
+    },
+    {
+      price: "£10",
+      drink: "Mojito",
+      quantity: "2x",
+      name: "Trof",
+      timerImg: require("../images/timer.png"),
+      venueImg: require("../images/trofIcon.jpg"),
+      type: "🍸",
+      duration: 950,
+      couponID: 12347
+    },
+    {
+      price: "£3",
+      drink: "Sheep Dip",
+      quantity: "1 Measure",
+      name: "Common",
+      timerImg: require("../images/timer.png"),
+      venueImg: require("../images/commonIcon.jpg"),
+      type: "🥃",
+      duration: 1000,
+      couponID: 12348
+    }
+  ]
+};
